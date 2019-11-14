5f6869132bb6659fc439253d4f96aee241d2a729
shoto-hatayama <hatayama.s00045@gmail.com>
2019-11-14 23:02:56 +0900

    画像クリック時、googlemap表示非表示設定

--- public/js/foods.js
+++ public/js/foods.js
@@ -5,6 +5,15 @@ $(function(){
 
     $('.flash_message').fadeOut(3000);
     
-    
+    $('img').on('click',function(){
+        var foodImg = $(this).attr('id');
+        shopMap = foodImg.replace('foodImg','shopMap');
+        if($('#'+shopMap).hasClass('d-none')){
+            $('#'+shopMap).removeClass('d-none');
+        }else{
+            $('#'+shopMap).addClass('d-none');    
+        }
+        
+    });
 
 });
