757b1d75c2d9458340b2f0b3137c189367399b36
shoto-hatayama <hatayama.s00045@gmail.com>
2019-11-14 23:05:50 +0900

    jsとcss読み込み処理を追記

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,10 +6,16 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="{{asset('css/modaal.min.css')}}">
-    <script src="{{asset('js/modaal.min.js')}}"></script>
-    <title>デリサーチ</title>
+    <script src="{{asset('js/remodal.js')}}"></script>
+    <script src="{{asset('js/foods.js')}}"></script>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{asset('css/remodal.css')}}">
+    <link rel="stylesheet" href="{{asset('css/remodal-default-theme.css')}}">
+    <title></title>
 </head>
+<header class="container-fluid bg-danger">
+    <div class="text-center h1 text-light">デリサーチ</div>
+</header>
 <body>
     @yield('content')
 </body>
