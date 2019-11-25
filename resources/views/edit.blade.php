47e69e6ee33d8f368fa3bf7b3d6f600332d68a51
shoto-hatayama <hatayama.s00045@gmail.com>
2019-11-25 12:58:54 +0900

    投稿内容編集・削除画面作成

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,62 @@
+@extends('layout')
+
+@section('content')
+
+<!-- フラッシュメッセージ表示-->
+@if(session('flash_message'))
+  <div class="flash_message bg-success  text-light text-center py-3 my-0">
+      {{ session('flash_message')}}
+  </div>
+@endif
+<!-- エラーメッセージ表示 -->
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+  
+<div class="container">
+  <form id="modal" action="edit" method="post" enctype="multipart/form-data">
+        @csrf
+        <div class="form-group">
+            <label for="inputShopName">店の名前</label>
+            <input type="text" class="form-control" id="inputShopName" name="shopName" value="{{ $data->shopName }}">
+        </div>
+        <div class="form-group">
+            <label for="inputFood">食べたもの</label>
+            <input type="text" class="form-control" id="inputFood" name="food" value="{{$data->food}}">
+        </div>
+        <div class="form-group">
+            <label for="inputLocation">場所</label>
+            <input type="text" class="form-control" id="inputLocation" name="location" value="{{ $data->location}}">
+        </div>
+        <div class="form-check">
+          <input class="form-check-input" type="checkbox" id="imgChangeCheck">
+          <label class="form-check-label" >画像を変更する</label>
+        </div>
+        <div class="form-group" id="changeImg">
+            <label>写真</label>
+            <input type="file" class="form-control-file" name="photo" value="{{ $data->photo}}" >
+        </div>
+        <div class="form-group">
+            <label for="inputUrl">URL</label>
+            <input type="text" class="form-control" name="url" value="{{ $data->url}}">
+        </div>
+        <div class="form-group">
+            <label for="inputComment">ひとこと</label>
+            <textarea type="text" class="form-control" id="inputComment" name="comment" value="{{ $data->comment}}"></textarea>
+        </div>
+        <!-- //ジャンルは未実装
+        <div class="form-group">
+            <label for="inputgenre">ジャンル</label>
+            <input type="text" class="form-control" id="inputgenre" name="genre">
+        </div> -->
+        <button data-remodal-action="cancel" class="remodal-cancel">閉じる</button>
+        <button data-remodal-action="confirm" class="remodal-confirm">登録</button>
+  </form>
+</div>
+@endsection
