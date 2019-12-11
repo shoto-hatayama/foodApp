7badc94b2cbfb53891c2d5022a01fef51ae5f478
shoto-hatayama <hatayama.s00045@gmail.com>
2019-12-11 14:16:36 +0900

    diskメソッドのミス修正

--- app/Http/Controllers/FoodController.php
+++ app/Http/Controllers/FoodController.php
@@ -35,7 +35,7 @@ class FoodController extends Controller
                 });
             $photoName = $file->getClientOriginalName();
             $filePath = '/'.config('folder').'/';
-            Storage:disk('dropbox')->put($filePath.$photoName,(string)$image->encode('jpg',100),'public');
+            Storage::disk('dropbox')->put($filePath.$photoName,(string)$image->encode('jpg',100),'public');
         }else{
             $photoName = "no_image.jpg";
         }
@@ -82,7 +82,7 @@ class FoodController extends Controller
                     });
                 $photoName = $file->getClientOriginalName();
                 $filePath = '/'.config('folder').'/';
-                Storage:disk('dropbox')->put($filePath.$photoName,(string)$image->encode('jpg',100),'public');
+                Storage::disk('dropbox')->put($filePath.$photoName,(string)$image->encode('jpg',100),'public');
                 $storagePath = Storage::disk('dropbox')->url($photoName);
                 
                 //以前の画像ファイルの削除
